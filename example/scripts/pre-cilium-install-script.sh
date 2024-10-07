fa819155ecd5ea68202cb1a91c3e31e6069ae536
Philip Schmid <phisch@cisco.com>
2024-10-07 14:52:09 +0200

    Repro Cilium 1.16.2 IPsec/Ingress issue
    Signed-off-by: Philip Schmid <phisch@cisco.com>

--- example/scripts/pre-cilium-install-script.sh
+++ example/scripts/pre-cilium-install-script.sh
@@ -8,4 +8,7 @@ do
   sleep 10
 done
 
+kubectl create -n kube-system secret generic cilium-ipsec-keys \
+  --from-literal=keys="3 rfc4106(gcm(aes)) $(echo $(dd if=/dev/urandom count=20 bs=1 2> /dev/null | xxd -p -c 64)) 128"
+
 # TBD
