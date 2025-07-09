4b5db08aaddbe8649dc8cd823d58ff428e7b373f
Philip Schmid <phisch@cisco.com>
2025-07-09 22:11:16 +0200

    CI: Minor improvements
    Signed-off-by: Philip Schmid <phisch@cisco.com>

--- test/conformance/tests/cilium-connectivity-test.sh
+++ test/conformance/tests/cilium-connectivity-test.sh
@@ -6,23 +6,10 @@ set -euxo pipefail
 export KUBECONFIG=$(terraform output --raw path_to_kubeconfig_file)
 # Gran the namespace in which Cilium was installed.
 CILIUM_NAMESPACE=$(terraform output --raw cilium_namespace)
-# Hubble Relay port
-HUBBLE_RELAY_PORT=4245
 # Cilium Connectivity tests namespace
 TEST_NAMESPACE="cilium-test"
 # All Cilium Connectivity tests namespaces
 NAMESPACES=("${TEST_NAMESPACE}" "${TEST_NAMESPACE}-1")
-
-# Run the connectivity tests.
-kubectl -n "${CILIUM_NAMESPACE}" rollout status deployment/hubble-relay
-kubectl -n "${CILIUM_NAMESPACE}" port-forward svc/hubble-relay 4245:80 &
-PID=$!
-
-# Wait while forwarded port will be available
-while ! nc -vz localhost ${HUBBLE_RELAY_PORT} > /dev/null 2>&1 ; do
-    echo "waiting for port-forward ..."
-    sleep 1
-done
 
 cilium status --wait
 
@@ -33,10 +20,9 @@ for ns in "${NAMESPACES[@]}"; do
     kubectl label ns $ns pod-security.kubernetes.io/warn=privileged
 done
 
-cilium connectivity test --namespace "${CILIUM_NAMESPACE}" --test-namespace "${TEST_NAMESPACE}"
+cilium connectivity test --namespace "${CILIUM_NAMESPACE}" --test-namespace "${TEST_NAMESPACE}" --hubble=false --flow-validation=disabled --test "!no-policies,!allow-all-except-world"
 
 trap '{
-    kill -9 "${PID}"
     for ns in "${NAMESPACES[@]}"; do
         kubectl delete ns $ns
     done
